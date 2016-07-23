5d78ed654f463b66626176ff40ab064f01d886a2
dkl <daniel.c.klauer@web.de>
2016-07-23 18:02:39 +0200

    build script: Combine DOS/Win32 scripts

--- contrib/release/crossbuild.sh
+++ contrib/release/crossbuild.sh
@@ -3,14 +3,20 @@ set -e
 
 . common.sh
 
-mkdir -p win32
-cd win32
+mkdir -p build
+cd build
 
 install_native="$PWD/install"
 install_win32="$install_native/i686-w64-mingw32"
+install_dos="$install_native/i586-pc-msdosdjgpp"
+mkdir -p "$install_dos"
 mkdir -p "$install_win32"
 if [ ! -e "$install_win32/mingw" ]; then
 	ln -s . "$install_win32/mingw"
+fi
+if [ ! -e "$install_dos/dev/env/DJDIR" ]; then
+	mkdir -p "$install_dos/dev/env"
+	ln -s ../../.. "$install_dos/dev/env/DJDIR"
 fi
 export PATH="$install_native/bin:$PATH"
 
@@ -25,6 +31,10 @@ version_mpc=1.0.3
 version_mpfr=3.1.4
 
 title_binutils=binutils-$version_binutils
+title_djbnu=bnu226sr3
+title_djcrx=djcrx205
+title_djgcc=gcc610s
+title_djlsr=djlsr205
 title_gcc=gcc-$version_gcc
 title_gmp=gmp-$version_gmp
 title_libffi=libffi-$version_libffi
@@ -33,6 +43,10 @@ title_mpc=mpc-$version_mpc
 title_mpfr=mpfr-$version_mpfr
 
 tarball_binutils=$title_binutils.tar.bz2
+tarball_djbnu=$title_djbnu.zip
+tarball_djcrx=$title_djcrx.zip
+tarball_djgcc=$title_djgcc.zip
+tarball_djlsr=$title_djlsr.zip
 tarball_gcc=$title_gcc.tar.bz2
 tarball_gmp=$title_gmp.tar.lz
 tarball_libffi=$title_libffi.tar.gz
@@ -41,6 +55,10 @@ tarball_mpc=$title_mpc.tar.gz
 tarball_mpfr=$title_mpfr.tar.xz
 
 my_fetch $tarball_binutils "http://ftpmirror.gnu.org/binutils/$tarball_binutils"
+my_fetch $tarball_djbnu    "ftp://ftp.fu-berlin.de/pc/languages/djgpp/current/v2gnu/$tarball_djbnu"
+my_fetch $tarball_djcrx    "ftp://ftp.fu-berlin.de/pc/languages/djgpp/current/v2/$tarball_djcrx"
+my_fetch $tarball_djgcc    "ftp://ftp.fu-berlin.de/pc/languages/djgpp/current/v2gnu/$tarball_djgcc"
+my_fetch $tarball_djlsr    "ftp://ftp.fu-berlin.de/pc/languages/djgpp/current/v2/$tarball_djlsr"
 my_fetch $tarball_gcc      "http://ftpmirror.gnu.org/gcc/$title_gcc/$tarball_gcc"
 my_fetch $tarball_gmp      "https://gmplib.org/download/gmp/$tarball_gmp"
 my_fetch $tarball_libffi   "ftp://sourceware.org/pub/libffi/$tarball_libffi"
@@ -59,6 +77,10 @@ else
 fi
 
 my_extract $title_binutils $tarball_binutils
+my_extract $title_djbnu    $tarball_djbnu
+my_extract $title_djcrx    $tarball_djcrx
+my_extract $title_djgcc    $tarball_djgcc
+my_extract $title_djlsr    $tarball_djlsr
 my_extract $title_fbc      $tarball_fbc
 my_extract $title_gcc      $tarball_gcc
 my_extract $title_gmp      $tarball_gmp
@@ -72,11 +94,30 @@ my_extract $title_mpfr     $tarball_mpfr
 do_patch() {
 	srcdirname="$1"
 	case "$srcdirname" in
+	$title_djbnu)
+		cp -R gnu/binutils-*/. .
+		rm -rf gnu manifest
+		chmod +x configure
+		;;
+
+	$title_djgcc)
+		cp -R gnu/gcc-*/. .
+		rm -rf gnu manifest
+		chmod +x configure
+
+		# Fix precheck for fixincludes
+		patch -p1 < ../../gcc-fixincludes-with-build-sysroot.patch
+
+		# Disable fixincludes
+		sed -i 's@\./fixinc\.sh@-c true@' gcc/Makefile.in
+
 	gcc-*)
 		# Fix precheck for fixincludes
 		patch -p1 < ../../gcc-fixincludes-with-build-sysroot.patch
+
 		# Disable fixincludes
 		sed -i 's@\./fixinc\.sh@-c true@' gcc/Makefile.in
+
 		;;
 	esac
 }
@@ -92,6 +133,8 @@ maybe_do_patch() {
 	fi
 }
 
+maybe_do_patch $title_djbnu
+maybe_do_patch $title_djgcc
 maybe_do_patch $title_gcc
 
 ################################################################################
@@ -118,6 +161,17 @@ do_build_autotools_win32() {
 	make install DESTDIR="$install_win32"
 }
 
+do_build_autotools_dos() {
+	local srcname="$1"
+	shift
+	../"$srcname"/configure \
+		--build=$build_triplet --host=i586-pc-msdosdjgpp \
+		--prefix= \
+		--enable-static --disable-shared "$@"
+	make
+	make install DESTDIR="$install_dos"
+}
+
 do_build() {
 	local buildname="$1"
 
@@ -132,6 +186,18 @@ do_build() {
 	$title_binutils-build-win32-to-win32)
 		do_build_autotools_win32 $title_binutils \
 			--target=i686-w64-mingw32 \
+			--disable-nls --disable-multilib --disable-werror
+		;;
+
+	$title_djbnu-build-native-to-dos)
+		do_build_autotools_native $title_djbnu \
+			--target=i586-pc-msdosdjgpp --with-sysroot="$install_dos" \
+			--disable-nls --disable-multilib --disable-werror
+		;;
+
+	$title_djbnu-build-dos-to-dos)
+		do_build_autotools_dos $title_djbnu \
+			--target=i586-pc-msdosdjgpp \
 			--disable-nls --disable-multilib --disable-werror
 		;;
 
@@ -150,6 +216,22 @@ do_build() {
 			--enable-wildcard
 		make
 		make install DESTDIR="$install_win32"
+		;;
+
+	$title_djcrx-build-dos)
+		cp -R ../$title_djcrx/. .
+		cp -R ../$title_djlsr/. .
+		find . -type f -name "*.o" -or -name "*.a" | xargs rm
+		cd src
+		rm -f *.opt
+		sed -i 's/-Werror//g' makefile.cfg
+		make
+		cd ..
+		cp -R include/. "$install_dos"/include
+		cp lib/*.a lib/*.o "$install_dos"/lib
+		install -m 0755 hostbin/stubify.exe "$install_native"/bin/stubify
+		install -m 0755 hostbin/stubedit.exe "$install_native"/bin/stubedit
+		install -m 0755 hostbin/dxegen.exe "$install_native"/bin/dxegen
 		;;
 
 	$title_gcc-build-native-to-win32-gcc)
@@ -212,6 +294,62 @@ do_build() {
 			--disable-win32-registry
 		;;
 
+	$title_djgcc-build-native-to-dos-gcc)
+		../$title_djgcc/configure \
+			--build=$build_triplet --host=$build_triplet --target=i586-pc-msdosdjgpp \
+			--prefix="$install_native" \
+			--with-local-prefix="$install_native" \
+			--with-sysroot="$install_dos" \
+			--with-gmp="$install_native" \
+			--with-mpfr="$install_native" \
+			--with-mpc="$install_native" \
+			--enable-static --disable-shared \
+			--disable-bootstrap --enable-languages=c,c++ \
+			--disable-nls --disable-multilib \
+			--disable-lto --disable-lto-plugin \
+			--disable-libssp --disable-libquadmath \
+			--disable-libmudflap --disable-libgomp --disable-libatomic \
+			--disable-decimal-float
+		make all-gcc
+		make install-gcc
+		;;
+
+	$title_djgcc-build-native-to-dos-full)
+		../$title_djgcc/configure \
+			--build=$build_triplet --host=$build_triplet --target=i586-pc-msdosdjgpp \
+			--prefix="$install_native" \
+			--with-local-prefix="$install_native" \
+			--with-sysroot="$install_dos" \
+			--with-gmp="$install_native" \
+			--with-mpfr="$install_native" \
+			--with-mpc="$install_native" \
+			--enable-static --disable-shared \
+			--disable-bootstrap --enable-languages=c,c++ \
+			--disable-nls --disable-multilib \
+			--disable-lto --disable-lto-plugin \
+			--disable-libssp --disable-libquadmath \
+			--disable-libmudflap --disable-libgomp --disable-libatomic \
+			--disable-decimal-float
+		make
+		make install
+		;;
+
+	$title_djgcc-build-dos-to-dos)
+		do_build_autotools_dos $title_djgcc \
+			--target=i586-pc-msdosdjgpp \
+			--with-local-prefix= \
+			--with-build-sysroot="$install_dos" \
+			--with-gmp="$install_dos" \
+			--with-mpfr="$install_dos" \
+			--with-mpc="$install_dos" \
+			--disable-bootstrap --enable-languages=c,c++ \
+			--disable-nls --disable-multilib \
+			--disable-lto --disable-lto-plugin \
+			--disable-libssp --disable-libquadmath \
+			--disable-libmudflap --disable-libgomp --disable-libatomic \
+			--disable-decimal-float
+		;;
+
 	fbc-*-build-native)
 		rm -f config.mk
 		echo 'V := 1'                                                 >> config.mk
@@ -250,6 +388,10 @@ do_build() {
 	$title_mpc-build-win32)    do_build_autotools_win32 $title_mpc  --with-gmp="$install_win32" --with-mpfr="$install_win32";;
 	$title_libffi-build-win32) do_build_autotools_win32 $title_libffi;;
 
+	$title_gmp-build-dos)    do_build_autotools_dos $title_gmp;;
+	$title_mpfr-build-dos)   do_build_autotools_dos $title_mpfr --with-gmp="$install_dos";;
+	$title_mpc-build-dos)    do_build_autotools_dos $title_mpc  --with-gmp="$install_dos" --with-mpfr="$install_dos";;
+
 	*)
 		echo "TODO: build $buildname"
 		exit 1
@@ -281,13 +423,17 @@ maybe_do_build() {
 
 # cross toolchain and needed target libs
 maybe_do_build $title_binutils-build-native-to-win32
+maybe_do_build $title_djbnu-build-native-to-dos
 maybe_do_build $title_gmp-build-native
 maybe_do_build $title_mpfr-build-native
 maybe_do_build $title_mpc-build-native
 maybe_do_build $title_mingww64-build-win32-headers
 maybe_do_build $title_gcc-build-native-to-win32-gcc
+maybe_do_build $title_djgcc-build-native-to-dos-gcc
 maybe_do_build $title_mingww64-build-win32-crt
+maybe_do_build $title_djcrx-build-dos
 maybe_do_build $title_gcc-build-native-to-win32-full
+maybe_do_build $title_djgcc-build-native-to-dos-full
 maybe_do_build $title_libffi-build-win32
 maybe_do_build fbc-$version_fbc-build-native
 
